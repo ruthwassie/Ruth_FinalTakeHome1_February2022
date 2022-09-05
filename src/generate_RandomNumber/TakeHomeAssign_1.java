5ef70cb432b2a46071a2739e5c7a0b40108cea7b
ruth wassie <ruth_wassie@yahoo.com>
2022-09-04 22:26:00 -0500

    first commit

---
+++ src/generate_RandomNumber/TakeHomeAssign_1.java
@@ -0,0 +1,38 @@
+package generate_RandomNumber;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TakeHomeAssign_1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		List<Integer> mydata = new ArrayList<Integer>();
+
+		Random randomNumber = new Random();
+		Random rand = new Random();
+
+		for (int i = 0; i < 500; i++) {
+
+			int randomNumbers = rand.nextInt(100, 900);
+			// System.out.println(randomNumbers);
+			mydata.add(randomNumbers);
+
+		}
+		Collections.sort(mydata);
+		System.out.println(mydata);
+		System.out.println("Enter Nth Number");
+		int NthNumber = sc.nextInt();
+
+		System.out.println("The" + NthNumber + "th samllest number is" + mydata.get(NthNumber - 1));
+
+	}
+
+}
